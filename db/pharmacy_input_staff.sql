56b06404ac6bb62c4047c851670d1314fbf93fd8
agungadipurwa <adiagung707@gmail.com>
2022-05-27 17:06:08 +0800

    Input some data of staff

---
+++ db/pharmacy_input_staff.sql
@@ -0,0 +1,34 @@
+USE batin_pharmacy
+
+INSERT INTO occupation (position)
+VALUES
+('admin'),
+('staff warehouse'),
+('staff frontline');
+
+INSERT INTO staff(username, image, gender, email, password, occupation_id)
+VALUES
+(
+'agung adipurwa',
+'https://ik.imagekit.io/j8loglunjkf/Twibbon_PrvnXfWeW.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1652426894362',
+'L',
+'adiagung707@gmail.com',
+'22032002',
+1
+);
+(
+'maysa putra',
+'https://ik.imagekit.io/j8loglunjkf/WhatsApp_Image_2022-05-13_at_16.10.13_ffjbBFkop.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1652433916214',
+'L',
+'maysaputrwa@gmail.com',
+'23052002',
+2
+),
+(
+'prami apsari',
+'https://ik.imagekit.io/j8loglunjkf/lena_yU9gSleAb.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1647764841545',
+'P',
+'pramiapsari@gmail.com',
+'19012002',
+3
+);
