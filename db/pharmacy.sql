c2298863d1daa594c9b6f0f2be853887d63353bb
Maysa <maysa@pop-os.localdomain>
2022-06-01 19:09:33 +0800

    update page fitur

--- db/pharmacy.sql
+++ db/pharmacy.sql
@@ -7,6 +7,8 @@
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
+CREATE Database batin_pharmacy;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
